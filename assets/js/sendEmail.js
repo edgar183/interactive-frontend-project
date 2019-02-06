774a721539efde8952bcb219871a47eca5e84bad
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-02-06 20:14:20 +0000

    the email validation function added

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,3 +1,16 @@
+const emailField = document.getElementById('emailaddress');
+const sendButton = document.getElementById('send-btn');
+
+emailField.addEventListener('keyup', function(event) {
+    isValidEmail = emailField.checkValidity();
+    if (isValidEmail) {
+        sendButton.disabled = false;
+    }
+    else {
+        sendButton.disabled = true;
+    }
+});
+
 function sendEmail(contactForm) {
     emailjs.send("gmail", "edgars_peskaitis", {
             "from_name": contactForm.name.value,
@@ -13,4 +26,5 @@ function sendEmail(contactForm) {
             }
         );
     return false;
+
 }
